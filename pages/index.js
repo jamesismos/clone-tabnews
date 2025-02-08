f6285c62c27b2522b5b535f101d0fb77d42761b5
James Oliveira <68132122+jamesismos@users.noreply.github.com>
2025-02-08 21:47:07 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Mariana, eu amo você. Se você me ama, dá uma risadinha!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste() {
-  console.log("indentação errada");
-}
-
 export default Home;
